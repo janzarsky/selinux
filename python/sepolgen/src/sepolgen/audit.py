526fe31dce1fc8bcda2ba96107b6e09c58b5e8bc
Jan Zarsky <jan.zarsky@gmail.com>
2018-03-13 08:26:06 +0100

    python/sepolgen: fix typo in audit

--- python/sepolgen/src/sepolgen/audit.py
+++ python/sepolgen/src/sepolgen/audit.py
@@ -239,7 +239,7 @@ class AVCMessage(AuditMessage):
             elif fields[0] == "name":
                 self.name = fields[1][1:-1]
             elif fields[0] == "ioctlcmd":
-                self.ioctlcmd = int(fields[1], 16);
+                self.ioctlcmd = int(fields[1], 16)
 
         if not found_src or not found_tgt or not found_class or not found_access:
             raise ValueError("AVC message in invalid format [%s]\n" % self.message)
