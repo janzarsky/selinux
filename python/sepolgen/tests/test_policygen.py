519b0f136fdfbaee9a3432e812d93e3a751eb668
Jan Zarsky <jan.zarsky@gmail.com>
2018-04-20 19:02:53 +0200

    test xperms generating

--- python/sepolgen/tests/test_policygen.py
+++ python/sepolgen/tests/test_policygen.py
@@ -26,6 +26,15 @@ class TestPolicyGenerator(unittest.TestCase):
     def setUp(self):
         self.g = policygen.PolicyGenerator()
 
+    def test_init(self):
+        self.assertFalse(self.g.xperms)
+
+    def test_set_gen_xperms(self):
+        self.g.set_gen_xperms(True)
+        self.assertTrue(self.g.xperms)
+        self.g.set_gen_xperms(False)
+        self.assertFalse(self.g.xperms)
+
     def test_av_rules(self):
         av1 = access.AccessVector(["test_src_t", "test_tgt_t", "file", "ioctl"])
         av2 = access.AccessVector(["test_src_t", "test_tgt_t", "file", "open"])
@@ -53,7 +62,7 @@ class TestPolicyGenerator(unittest.TestCase):
         av2 = access.AccessVector(["test_src_t", "test_tgt_t", "file", "ioctl"])
         av2.xperms['ioctl'] = refpolicy.XpermSet()
         av2.xperms['ioctl'].add(1234)
-        av3 = access.AccessVector(["test_src_t", "test_tgt_t", "file", "ioctl"])
+        av3 = access.AccessVector(["test_src_t", "test_tgt_t", "dir", "ioctl"])
         av3.xperms['ioctl'] = refpolicy.XpermSet()
         av3.xperms['ioctl'].add(2345)
 
@@ -64,14 +73,56 @@ class TestPolicyGenerator(unittest.TestCase):
 
         self.g.add_access(avs)
 
-        self.assertEqual(len(self.g.module.children), 2)
+        self.assertEqual(len(self.g.module.children), 4)
 
-        r1 = self.g.module.children[0]
-        self.assertIsInstance(r1, refpolicy.AVRule)
-        self.assertEqual(r1.to_string(),
-            "allow test_src_t test_tgt_t:file ioctl;")
+        # we cannot sort the rules, so find all rules manually
+        av_rule1 = av_rule2 = av_ext_rule1 = av_ext_rule2 = None
 
-        r2 = self.g.module.children[1]
-        self.assertIsInstance(r2, refpolicy.AVExtRule)
-        self.assertEqual(r2.to_string(),
-            "allowxperm test_src_t test_tgt_t:file ioctl { 42 1234 2345 };")
+        for r in self.g.module.children:
+            if isinstance(r, refpolicy.AVRule):
+                if 'file' in r.obj_classes:
+                    av_rule1 = r
+                else:
+                    av_rule2 = r
+            elif isinstance(r, refpolicy.AVExtRule):
+                if 'file' in r.obj_classes:
+                    av_ext_rule1 = r
+                else:
+                    av_ext_rule2 = r
+            else:
+                self.fail("Unexpected rule type '%s'" % type(r))
+
+        # check that all rules are present
+        self.assertNotIn(None, (av_rule1, av_rule2, av_ext_rule1, av_ext_rule2))
+
+        self.assertEqual(av_rule1.rule_type, av_rule1.ALLOW)
+        self.assertEqual(av_rule1.src_types, {"test_src_t"})
+        self.assertEqual(av_rule1.tgt_types, {"test_tgt_t"})
+        self.assertEqual(av_rule1.obj_classes, {"file"})
+        self.assertEqual(av_rule1.perms, {"ioctl"})
+
+        self.assertEqual(av_ext_rule1.rule_type, av_ext_rule1.ALLOWXPERM)
+        self.assertEqual(av_ext_rule1.src_types, {"test_src_t"})
+        self.assertEqual(av_ext_rule1.tgt_types, {"test_tgt_t"})
+        self.assertEqual(av_ext_rule1.obj_classes, {"file"})
+        self.assertEqual(av_ext_rule1.operation, "ioctl")
+        xp1 = refpolicy.XpermSet()
+        xp1.add(42)
+        xp1.add(1234)
+        self.assertEqual(av_ext_rule1.xperms.ranges, xp1.ranges)
+
+        self.assertEqual(av_rule2.rule_type, av_rule2.ALLOW)
+        self.assertEqual(av_rule2.src_types, {"test_src_t"})
+        self.assertEqual(av_rule2.tgt_types, {"test_tgt_t"})
+        self.assertEqual(av_rule2.obj_classes, {"dir"})
+        self.assertEqual(av_rule2.perms, {"ioctl"})
+
+        self.assertEqual(av_ext_rule2.rule_type, av_ext_rule2.ALLOWXPERM)
+        self.assertEqual(av_ext_rule2.src_types, {"test_src_t"})
+        self.assertEqual(av_ext_rule2.tgt_types, {"test_tgt_t"})
+        self.assertEqual(av_ext_rule2.obj_classes, {"dir"})
+        self.assertEqual(av_ext_rule2.operation, "ioctl")
+        xp2 = refpolicy.XpermSet()
+        xp2.add(2345)
+        self.assertEqual(av_ext_rule2.xperms.ranges, xp2.ranges)
+
