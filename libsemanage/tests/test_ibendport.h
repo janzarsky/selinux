bf262fb641434e406f5ee8acec5702c6fe2f2e5a
Jan Zarsky <jzarsky@redhat.com>
2019-06-06 15:00:36 +0200

    Fix formatting of test_ibendport

--- libsemanage/tests/test_ibendport.h
+++ libsemanage/tests/test_ibendport.h
@@ -3,22 +3,18 @@
 
 #include <CUnit/Basic.h>
 
-#include "utilities.h"
-
 int ibendport_test_init(void);
 int ibendport_test_cleanup(void);
 int ibendport_add_tests(CU_pSuite suite);
 
-// ibendports_policy.h
-
+/* ibendports_policy.h */
 void test_ibendport_query(void);
 void test_ibendport_exists(void);
 void test_ibendport_count(void);
 void test_ibendport_iterate(void);
 void test_ibendport_list(void);
 
-// ibendports_local.h
-
+/* ibendports_local.h */
 void test_ibendport_modify_del_query_local(void);
 void test_ibendport_exists_local(void);
 void test_ibendport_count_local(void);
