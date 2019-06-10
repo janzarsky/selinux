c1648d1a1ef9fe2d27376655a0d3b99397284eed
Jan Zarsky <jzarsky@redhat.com>
2019-06-10 14:09:03 +0200

    Fix formatting of test_other

--- libsemanage/tests/test_other.h
+++ libsemanage/tests/test_other.h
@@ -3,19 +3,14 @@
 
 #include <CUnit/Basic.h>
 
-#include "utilities.h"
-
 int other_test_init(void);
 int other_test_cleanup(void);
 int other_add_tests(CU_pSuite suite);
 
-// context_record.h
-
+/* context_record.h */
 void test_semanage_context(void);
 
-// debug.h
-
+/* debug.h */
 void test_debug(void);
 
 #endif
-
